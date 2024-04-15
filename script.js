4c9130cfb8b0efc9411b891cb212595a081929d4
endo <endofy.01@gmail.com>
2024-04-16 00:10:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const mobile_nav = document.querySelector(".mobileNavBtn");
+const nav_header = document.querySelector(".header");
+const nav_Links = document.querySelector(".navLinks");
+const toggleNav = () => {
+    nav_header.classList.toggle("active");
+    
+};
+const closeNav = () => {
+    nav_header.classList.remove("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNav());
+
+nav_Links.addEventListener("click", () => closeNav());
